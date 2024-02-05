949a3b469a7d6a0e6718d613a7edb93c14bc9069
stevennedwinn <stevennedwinn@gmail.com>
2024-02-06 02:27:25 +0700

    [UPDATE] move imu.ino to folder

--- imu_gps/imu/imu.ino
+++ imu_gps/imu/imu.ino
@@ -5,8 +5,8 @@
 
 // VCC to 3.3V
 // GND to GND
-// SDA to analog4
-// SCL to analog5
+// SDA to analog4 for uno
+// SCL to analog5 for uno
 
 BNO080 myIMU;
 ros::NodeHandle nh;
@@ -19,6 +19,17 @@ void setup() {
   nh.advertise(imu_pub);
 
   myIMU.begin(); // Initialize BNO080
+
+  Wire.setClock(400000); //Increase I2C data rate to 400kHz
+
+  myIMU.enableAccelerometer(50);    //We must enable the accel in order to get MEMS readings even if we don't read the reports.
+  myIMU.enableRawAccelerometer(50); //Send data update every 50ms
+  myIMU.enableGyro(50);
+  myIMU.enableRawGyro(50);
+  myIMU.enableMagnetometer(50);
+  myIMU.enableRawMagnetometer(50);
+
+  Serial.println(F("Raw MEMS readings enabled"));
 }
 
 void loop() {
